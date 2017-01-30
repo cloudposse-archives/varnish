60bbad8a1c1e4be9b1ca35da113208bfb767d23b
Igor Rodionov <goruha@gmail.com>
2017-01-30 15:49:44 +0600

    Added final alpine image

---
+++ rootfs/init.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo ${VARNISH_SECRET} > /etc/varnish/secret
+
+set -x
+
+OPS=()
+OPS=("-j none")
+OPS+=("-F")
+OPS+=("-a :80")
+OPS+=("-T :6082")
+OPS+=("-S /etc/varnish/secret")
+OPS+=("-p thread_pool_min=${VARNISH_THREAD_POOL_MIN}")
+OPS+=("-p thread_pools=${VARNISH_THREAD_POOLS}")
+OPS+=("-p cli_timeout=${VARNISH_CLI_TIMEOUT}")
+OPS+=("-p first_byte_timeout=${VARNISH_BACKEND_FIRST_BYTE_TIMEOUT}")
+OPS+=("-p connect_timeout=${VARNISH_BACKEND_CONNECT_TIMEOUT}")
+OPS+=("-p between_bytes_timeout=${VARNISH_BACKEND_BETWEEN_BYTES_TIMEOUT}")
+OPS+=("-s file,/var/lib/varnish/varnish_storage.bin,${VARNISH_STORAGE}")
+
+if [[ -a /etc/varnish/default.vcl ]]; then
+
+OPS+=("-f  /etc/varnish/default.vcl")
+
+else
+
+OPS+=("-b ${VARNISH_BACKEND_HOST}:${VARNISH_BACKEND_PORT}")
+
+fi
+
+varnishd ${OPS[*]};
+
