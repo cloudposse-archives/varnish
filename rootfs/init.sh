cfe1105df5c250b31cc324af0bd458982f3d2a43
Igor Rodionov <goruha@gmail.com>
2017-01-30 19:06:03 +0600

    Change config path

--- rootfs/init.sh
+++ rootfs/init.sh
@@ -9,7 +9,7 @@ OPS=("-j none")
 OPS+=("-F")
 OPS+=("-a :80")
 OPS+=("-T :6082")
-OPS+=("-S /etc/varnish/secret")
+OPS+=("-S /etc/varnish/secrets/secret")
 OPS+=("-p thread_pool_min=${VARNISH_THREAD_POOL_MIN}")
 OPS+=("-p thread_pools=${VARNISH_THREAD_POOLS}")
 OPS+=("-p cli_timeout=${VARNISH_CLI_TIMEOUT}")
@@ -18,9 +18,9 @@ OPS+=("-p connect_timeout=${VARNISH_BACKEND_CONNECT_TIMEOUT}")
 OPS+=("-p between_bytes_timeout=${VARNISH_BACKEND_BETWEEN_BYTES_TIMEOUT}")
 OPS+=("-s file,/var/lib/varnish/varnish_storage.bin,${VARNISH_STORAGE}")
 
-if [[ -a /etc/varnish/default.vcl ]]; then
+if [[ -a /etc/varnish/configs/default.vcl ]]; then
 
-OPS+=("-f  /etc/varnish/default.vcl")
+OPS+=("-f  /etc/varnish/configs/default.vcl")
 
 else
 
