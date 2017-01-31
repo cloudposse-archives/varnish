c328d89228ac893b32be739ff84d4fbfcedfea9b
Igor Rodionov <goruha@gmail.com>
2017-01-31 15:53:32 +0600

    Fix docker image

--- rootfs/init.sh
+++ rootfs/init.sh
@@ -13,10 +13,6 @@ OPS+=("-T :6082")
 OPS+=("-S /etc/varnish/secrets/secret")
 OPS+=("-p thread_pool_min=${VARNISH_THREAD_POOL_MIN}")
 OPS+=("-p thread_pools=${VARNISH_THREAD_POOLS}")
-OPS+=("-p cli_timeout=${VARNISH_CLI_TIMEOUT}")
-OPS+=("-p first_byte_timeout=${VARNISH_BACKEND_FIRST_BYTE_TIMEOUT}")
-OPS+=("-p connect_timeout=${VARNISH_BACKEND_CONNECT_TIMEOUT}")
-OPS+=("-p between_bytes_timeout=${VARNISH_BACKEND_BETWEEN_BYTES_TIMEOUT}")
 OPS+=("-s file,/var/lib/varnish/varnish_storage.bin,${VARNISH_STORAGE}")
 
 if [[ -a /etc/varnish/configs/default.vcl ]]; then
